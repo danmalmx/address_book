a794f2ebbe7fe14b551456a6e6cb5480fa2a678b
danmalmx <danmalmx@gmail.com>
2018-09-04 13:37:56 +0200

    commit to show errors - can't get 1st test to pass

---
+++ features/step_definitions/basic-steps.js
@@ -0,0 +1,29 @@
+const { After, Given, Then, When } = require("cucumber");
+
+After(async function() {
+    return await this.closedHomePage()
+})
+
+Given("I visit the site", async function() {
+  return await this.openHomePage()
+})
+
+Then("I should see {string}", async function(content) {
+  return "pending";
+})
+
+When('I click {string}', async function(string) {
+    return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+    return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+    return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+    return 'pending'
+})
