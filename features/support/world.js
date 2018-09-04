e0edc3f375afd839277bc2f3c849451f4a78d8f9
danmalmx <danmalmx@gmail.com>
2018-09-04 15:37:34 +0200

    Teste checking content passed

--- features/support/world.js
+++ features/support/world.js
@@ -16,6 +16,13 @@ class AddressBookWorld {
         async closeHomePage() {
             await this.browser.close()   
             }
+
+        async pageHasTextContent(expectedContent) {
+            const pageContent = await this.page.content()
+            const actualContent = pageContent.match(expectedContent)[0]
         }
+
+        
+    }
 
 setWorldConstructor(AddressBookWorld);
