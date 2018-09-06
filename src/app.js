4cc0c9e6bbc4f1bc5b90b9306dd4e6546eacdfc4
danmalmx <danmalmx@gmail.com>
2018-09-06 20:15:57 +0200

    update

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ if (contacts) {
     contacts.forEach(contact => {
         let li = document.createElement('li')
         li.innerHTML = `
-          <div class="card" class="bg-teal mx-auto max-w-lg shadow-lg rounded-lg overflow-hidden">
-            <div class="image" class="sm:flex sm:items-center px-6 py-4">
+          <div class="card bg-teal mx-auto max-w-lg shadow-lg rounded-lg overflow-hidden">
+            <div class="image m:flex sm:items-center px-6 py-4">
               <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://lh3.googleusercontent.com/Y9fTQTS97Um1XW3vN9UNH_YNEol5BtjoeLlkqWamegZn7yCDXG5CxHA1Dd37oDa4tQAl39S4t0Fl4A_Rqz9exNSSs2DOgQsqdoJnnIGK-2jLmbEntVFqDMysEldee1tbE3Y1m_BboZCB_Alw2WgxSCOoWOXK8lZZ5ikyqjCWa4l9_5KP-gGC8ddokgF5SRu2qPaXeZhe68Sx00pPiUvF4P4-bSdpf5O6Vrs8NrobFaHvBYzz-1o7jTsziffa8HKe5W_HIK_UQ88t0driJSK9GNtYcmh2D0B_uAITbU8W6hf4Vw6Utb8U3sDCp5lCE8WGiRDvqK_TBx0K6bxuXRtFa9OdIilC2l7Q2vxXY76RkZLgjIIX3Oh4qsngfCHfXrszB2lLOBcS62r98asR7DoxXLMXC0547oT0FbTAlZpm3ElUqryIvWsPOytdezwqGwY1ujxpyTwQ1yXRJKi4p3dQY1lNb56zbQ8LUL7UbJlutNmZY-bpcxoOPlx6rvjRnMYI9NzJhl_Ek0woksDYUmSF1UjhUDDOKOFh1GIYQSmvo8MCE5vSvKSh2JxxHcI1itDnwTRlkSewPxualXplD9jfMNIvceli4hMHpxeJKIzG1lwz7znWxnCJ2K5mNjKLRPJW=s766-no" alt="image" />
               <div class="text-center sm:text-left sm:flex-grow">            
             <div class="content mb-4 pt-6">
