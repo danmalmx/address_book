3bea4f530581620b28450b10e86c2cb597181ba9
danmalmx <danmalmx@gmail.com>
2018-09-07 14:05:38 +0200

    hide-form functionality breaks code

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ if (contacts) {
         li.innerHTML = `
         <br>
         <div class="card bg-teal mx-auto max-w-lg shadow-lg rounded-lg overflow-hidden">
-        <div class="image m:flex sm:items-center px-4 py-2">
-          <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://lh3.googleusercontent.com/Y9fTQTS97Um1XW3vN9UNH_YNEol5BtjoeLlkqWamegZn7yCDXG5CxHA1Dd37oDa4tQAl39S4t0Fl4A_Rqz9exNSSs2DOgQsqdoJnnIGK-2jLmbEntVFqDMysEldee1tbE3Y1m_BboZCB_Alw2WgxSCOoWOXK8lZZ5ikyqjCWa4l9_5KP-gGC8ddokgF5SRu2qPaXeZhe68Sx00pPiUvF4P4-bSdpf5O6Vrs8NrobFaHvBYzz-1o7jTsziffa8HKe5W_HIK_UQ88t0driJSK9GNtYcmh2D0B_uAITbU8W6hf4Vw6Utb8U3sDCp5lCE8WGiRDvqK_TBx0K6bxuXRtFa9OdIilC2l7Q2vxXY76RkZLgjIIX3Oh4qsngfCHfXrszB2lLOBcS62r98asR7DoxXLMXC0547oT0FbTAlZpm3ElUqryIvWsPOytdezwqGwY1ujxpyTwQ1yXRJKi4p3dQY1lNb56zbQ8LUL7UbJlutNmZY-bpcxoOPlx6rvjRnMYI9NzJhl_Ek0woksDYUmSF1UjhUDDOKOFh1GIYQSmvo8MCE5vSvKSh2JxxHcI1itDnwTRlkSewPxualXplD9jfMNIvceli4hMHpxeJKIzG1lwz7znWxnCJ2K5mNjKLRPJW=s766-no" alt="image" />
+        <div class="image lg:flex md:flex md:items-center sm:items-center px-4 py-2">
+          <img class="lg:content-center md:content-center block sm:content-center block h-24 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-2 sm:ml-0" src="https://lh3.googleusercontent.com/Y9fTQTS97Um1XW3vN9UNH_YNEol5BtjoeLlkqWamegZn7yCDXG5CxHA1Dd37oDa4tQAl39S4t0Fl4A_Rqz9exNSSs2DOgQsqdoJnnIGK-2jLmbEntVFqDMysEldee1tbE3Y1m_BboZCB_Alw2WgxSCOoWOXK8lZZ5ikyqjCWa4l9_5KP-gGC8ddokgF5SRu2qPaXeZhe68Sx00pPiUvF4P4-bSdpf5O6Vrs8NrobFaHvBYzz-1o7jTsziffa8HKe5W_HIK_UQ88t0driJSK9GNtYcmh2D0B_uAITbU8W6hf4Vw6Utb8U3sDCp5lCE8WGiRDvqK_TBx0K6bxuXRtFa9OdIilC2l7Q2vxXY76RkZLgjIIX3Oh4qsngfCHfXrszB2lLOBcS62r98asR7DoxXLMXC0547oT0FbTAlZpm3ElUqryIvWsPOytdezwqGwY1ujxpyTwQ1yXRJKi4p3dQY1lNb56zbQ8LUL7UbJlutNmZY-bpcxoOPlx6rvjRnMYI9NzJhl_Ek0woksDYUmSF1UjhUDDOKOFh1GIYQSmvo8MCE5vSvKSh2JxxHcI1itDnwTRlkSewPxualXplD9jfMNIvceli4hMHpxeJKIzG1lwz7znWxnCJ2K5mNjKLRPJW=s766-no" alt="image" />
           <div class="text-center sm:text-left sm:flex-grow">            
         <div class="content mb-2 pt-2">
-          <h1 class="text-md leading-tight">${ contact.name }</h1>
+          <h1 class="lg:flex- align-middle md:item-right align-middle sm:align-middle text-md leading-tight">${ contact.name }</h1>
           <h2 class="text-sm leading-tight text-grey-darkest">${ contact.company }</h2>
           <p class="text-sm leading-tight text-grey-darkest">${ contact.notes }</p> 
           <p class="text-sm leading-tight text-grey-darkest">${ contact.email } | 
